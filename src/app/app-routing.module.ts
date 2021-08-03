7699c48d865722d557039d5d40755aa33c11c2f1
daianaiurescu <daiana.iurescu@gmail.com>
2021-08-03 14:55:36 +0300

    basic auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
+import {AuthGuard} from "./services/auth.guard";
 
-const routes: Routes = []
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {AuthService} from "../services/auth.service";
+import {Route, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  logIn(username: HTMLInputElement, password: HTMLInputElement){
+    this.httpClient.get(`${environment.apiUrl}/login`, {
+      headers: {
+        Authorization: `Basic ${btoa(`${username.value}:${password.value}`)}`
+      },
+      observe: 'response'
+    }).subscribe(response => {
+        if (response.status >= 200 && response.status <= 204) {
+          this.authService.setToken(username.value, password.value);
+        }
+        this.router.navigate(['/home']);
+      });
+  }
 }
